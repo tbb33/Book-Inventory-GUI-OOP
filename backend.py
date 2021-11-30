a7e878e11324a9e7186f0037d451edc0c4255301
tbb33 <91215510+tbb33@users.noreply.github.com>
2021-11-29 22:48:06 -0600

    Use OOP to remove repetition
    In backend I was opening/closing db connection in each func/method.
    Solu: have connection and cursor object in __init__ function only

--- backend.py
+++ backend.py
@@ -13,38 +13,28 @@ class Database:
         conn.close()
 
     def add(self, title, author, year, isbn):
-        conn = sqlite3.connect("book.db")
-        cur = conn.cursor()
         cur.execute("INSERT INTO book VALUES (NULL,?,?,?,?)",(title, author, year, isbn))
         conn.commit()
         conn.close()
 
     def view(self):
-        conn = sqlite3.connect("book.db")
-        cur = conn.cursor()
         cur.execute("SELECT * FROM book")
         rows = cur.fetchall()
         conn.close()
         return rows
 
     def search(self, title="", author="", year="", isbn=""):
-        conn = sqlite3.connect("book.db")
-        cur = conn.cursor()
         cur.execute("SELECT * FROM book WHERE title=? OR author=? or year=? or isbn=?",(title, author, year, isbn))
         rows=cur.fetchall()
         conn.close()
         return rows
 
     def delete(self, id):
-        conn = sqlite3.connect("book.db")
-        cur = conn.cursor()
         cur.execute("DELETE FROM book WHERE id=?",(id,))
         conn.commit()
         conn.close()
 
     def update(self, id, title, author, year, isbn):
-        conn = sqlite3.connect("book.db")
-        cur = conn.cursor()
         cur.execute("UPDATE book set title=?, author=?, year=?, isbn=? WHERE id=?",(title, author, year, isbn, id))
         conn.commit()
         conn.close()
