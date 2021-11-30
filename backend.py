d3e95977a82edce18bca6992d9400d23eca27df7
tbb33 <91215510+tbb33@users.noreply.github.com>
2021-11-30 12:03:58 -0600

    Fix error "closed db"
    Prev code resulted in error "cannot operate on a closed db" when clicked any buttons
    Explanation: this occurred bc I closed to connection in each function so the connection was closed after each button press
    Solu: deleted closing the connection from ea func for now

--- backend.py
+++ backend.py
@@ -14,26 +14,21 @@ class Database:
     def add(self, title, author, year, isbn):
         self.cur.execute("INSERT INTO book VALUES (NULL,?,?,?,?)",(title, author, year, isbn))
         self.conn.commit()
-        self.conn.close()
 
     def view(self):
         self.cur.execute("SELECT * FROM book")
         rows = self.cur.fetchall()
-        self.conn.close()
         return rows
 
     def search(self, title="", author="", year="", isbn=""):
         self.cur.execute("SELECT * FROM book WHERE title=? OR author=? or year=? or isbn=?",(title, author, year, isbn))
         rows=self.cur.fetchall()
-        self.conn.close()
         return rows
 
     def delete(self, id):
         self.cur.execute("DELETE FROM book WHERE id=?",(id,))
         self.conn.commit()
-        self.conn.close()
 
     def update(self, id, title, author, year, isbn):
         self.cur.execute("UPDATE book set title=?, author=?, year=?, isbn=? WHERE id=?",(title, author, year, isbn, id))
         self.conn.commit()
-        self.conn.close()
