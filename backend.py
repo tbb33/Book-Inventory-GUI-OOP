5ebc99be09e27108c90c889eff158e4d3c32fe25
tbb33 <91215510+tbb33@users.noreply.github.com>
2021-11-29 21:45:11 -0600

    Create Database Class and init func
    __init__ function is executed when class is called (which will be called from frontend.py) and it initializes the minimal object which will:
    Est connection to db, create cursor obj, check if table exists

--- backend.py
+++ backend.py
@@ -1,58 +1,50 @@
 import sqlite3
 
-def connect():
-    conn = sqlite3.connect("book.db")
-    cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, isbn INTEGER )")
-    conn.commit()
-    conn.close()
+class Database:
 
-def add(title, author, year, isbn):
-    conn = sqlite3.connect("book.db")
-    cur = conn.cursor()
-    cur.execute("INSERT INTO book VALUES (NULL,?,?,?,?)",(title, author, year, isbn))
-    conn.commit()
-    conn.close()
+    #function executes when class is called
+    #initialize the  minimal object (est conn to db, create cursor obj,
+    #check if tbl exists)
+    def __init__():
+        conn = sqlite3.connect("book.db")
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, isbn INTEGER )")
+        conn.commit()
+        conn.close()
 
-def view():
-    conn = sqlite3.connect("book.db")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM book")
-    rows = cur.fetchall()
-    conn.close()
-    return rows
+    def add(title, author, year, isbn):
+        conn = sqlite3.connect("book.db")
+        cur = conn.cursor()
+        cur.execute("INSERT INTO book VALUES (NULL,?,?,?,?)",(title, author, year, isbn))
+        conn.commit()
+        conn.close()
 
-def search(title="", author="", year="", isbn=""):
-    conn = sqlite3.connect("book.db")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM book WHERE title=? OR author=? or year=? or isbn=?",(title, author, year, isbn))
-    rows=cur.fetchall()
-    conn.close()
-    return rows
+    def view():
+        conn = sqlite3.connect("book.db")
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM book")
+        rows = cur.fetchall()
+        conn.close()
+        return rows
 
-def delete(id):
-    conn = sqlite3.connect("book.db")
-    cur = conn.cursor()
-    cur.execute("DELETE FROM book WHERE id=?",(id,))
-    conn.commit()
-    conn.close()
+    def search(title="", author="", year="", isbn=""):
+        conn = sqlite3.connect("book.db")
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM book WHERE title=? OR author=? or year=? or isbn=?",(title, author, year, isbn))
+        rows=cur.fetchall()
+        conn.close()
+        return rows
 
-def update(id, title, author, year, isbn):
-    conn = sqlite3.connect("book.db")
-    cur = conn.cursor()
-    cur.execute("UPDATE book set title=?, author=?, year=?, isbn=? WHERE id=?",(title, author, year, isbn, id))
-    conn.commit()
-    conn.close()
+    def delete(id):
+        conn = sqlite3.connect("book.db")
+        cur = conn.cursor()
+        cur.execute("DELETE FROM book WHERE id=?",(id,))
+        conn.commit()
+        conn.close()
 
-
-connect() #will be executed anytime this script is ran (ie when run frontend)
-#TESTING FUNCTIONS
-# add("Planets", "Peter Vanguard", 1888, 67858998)
-# print(view())
-# add("Earth", "John Smith", 1799, 99654895)
-# print(view())
-# print(search(author="John Smith"))
-# delete(2)
-# print(view())
-update(1,"Space","Mary Matthews", 1919, 46215478)
-print(view())
+    def update(id, title, author, year, isbn):
+        conn = sqlite3.connect("book.db")
+        cur = conn.cursor()
+        cur.execute("UPDATE book set title=?, author=?, year=?, isbn=? WHERE id=?",(title, author, year, isbn, id))
+        conn.commit()
+        conn.close()
